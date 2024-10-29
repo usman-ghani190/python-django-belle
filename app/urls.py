b887f95b24fd99cf8b3d82786d976379553b7f72
Usman <Cap.usman21@gmail.com>
2024-10-30 00:00:42 +0500

    Remove virtual environment from repository

--- app/urls.py
+++ app/urls.py
@@ -6,4 +6,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shop/', views.shop_page, name='shop'),
+    path('shop/product', views.product_page, name='product'),
+    path('shop/cart', views.cart_page, name='cart'),
+
 ]
--- app/views.py
+++ app/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 def index(request):
     context= {}
     return render(request, 'app/index.html', context)
+
+def shop_page(request):
+    context= {}
+    return render(request, 'app/shop.html', context)
+
+def product_page(request):
+    context= {}
+    return render(request, 'app/product_page.html', context)
+
+def cart_page(request):
+    context= {}
+    return render(request, 'app/cart.html', context)
